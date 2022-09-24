43dc2ba1c43224ca41afed6d59e8893dbd5f94df
DamianZygma <damian.zygma@gmail.com>
2022-09-24 12:41:21 +0200

    initial commit

---
+++ AdviceService/src/main/java/pl/com/mtd/AdviceService/AdviceServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.com.mtd.AdviceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviceServiceApplication.class, args);
+	}
+
+}
---
+++ AdviceService/src/test/java/pl/com/mtd/AdviceService/AdviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.mtd.AdviceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class AdviceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
